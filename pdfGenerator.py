97ea68e72898df2a3ceb7c6cba5d95430b0186db
David Mendoza <d.mendoza99@unitec.edu>
2021-01-10 19:32:58 -0600

    works great too

--- pdfGenerator.py
+++ pdfGenerator.py
@@ -9,16 +9,16 @@ A4_HEIGHT = 297
 
 print('Started generating all pdfs...')
 for i in range(6):
-    print(f'Generating comic\'s #%s pdf... %d' % i+1)
+    print(f'Generating comic #%s pdf...' % str(i))
     pdf = FPDF()
-    lista = os.listdir(f'%s/%s%d' % (IMG_FOLDER_NAME, COMIC_NAME, i))
+    lista = os.listdir(f'%s/%s%s' % (IMG_FOLDER_NAME, COMIC_NAME, str(i)))
     lista.sort(key=lambda x: int(x.replace(".jpg", "")))
     for img in lista:
-        imagePath = f'%s/%s%d/%s' % (IMG_FOLDER_NAME, COMIC_NAME, i+1, img)
+        imagePath = f'%s/%s%s/%s' % (IMG_FOLDER_NAME, COMIC_NAME, str(i), img)
         pdf.add_page()
         pdf.image(imagePath, x=0, y=0, w=A4_WIDTH, h=A4_HEIGHT)
-    fileName = f'%s/Scott Pilgrim - %d.pdf' % (PDF_FOLDER_NAME, i+1)
+    fileName = f'%s/Scott Pilgrim - %s.pdf' % (PDF_FOLDER_NAME, str(i+1))
     os.makedirs(os.path.dirname(fileName), exist_ok=True)
     pdf.output(fileName, 'F')
-    print(f'Finished generating comic\'s #%d pdfs!' % i+1)
+    print(f'Finished generating comic #%s pdf!' % str(i+1))
 print(f'Finished generating all pdfs!')
